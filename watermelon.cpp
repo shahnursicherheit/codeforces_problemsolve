1957df2085b0d32c42f4dbde2388af3c72f47278
Md Shahnur <96790202+shahnursicherheit@users.noreply.github.com>
2023-09-18 01:05:53 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+        if(w%2==0&&w>2)
+        {
+            cout<<"yes\n";
+        }
+        else
+        cout<<"NO\n";
+    return 0;
+}
